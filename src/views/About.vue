6eedc47a5caa6eacd7861a211151337dfc6805d3
dimasliva <janzerikYT@yandex.ru>
2021-12-29 09:06:16 +0500

    'fix'

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>My github</h1>
+    <a target="_blank" href="https://github.com/dimasliva/shopvue">Click</a>
   </div>
 </template>
